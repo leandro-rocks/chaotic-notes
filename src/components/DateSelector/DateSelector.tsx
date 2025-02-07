e121d3e1c596a570217b6472a03d82ca7d875793
LeandroPS <leandro.pires.souza@gmail.com>
2025-02-07 17:49:13 -0300

    feat: enhance DateSelector component with week navigation and styled items

--- src/components/DateSelector/DateSelector.tsx
+++ src/components/DateSelector/DateSelector.tsx
@@ -1,38 +1,80 @@
-import { useDates } from "../../hooks/useDates";
+import { useDates } from "@hooks/useDates";
 import { useEffect, useState } from "react";
+import {
+  DateItemContainer,
+  DateItem,
+  Wrapper,
+  IconButtonWrapper,
+} from "./styles";
+import Icon from "@components/Icon";
 
 const DateSelector = () => {
   const { currentDate, setCurrentDate } = useDates();
   const [dates, setDates] = useState<Date[]>([]);
 
   useEffect(() => {
+    const startOfWeek = new Date(currentDate);
+    startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
+
+    const endOfWeek = new Date(currentDate);
+    endOfWeek.setDate(endOfWeek.getDate() + (6 - endOfWeek.getDay()));
+
     const datList = [];
 
-    for (let i = -3; i < 4; i++) {
-      const date = new Date(currentDate);
-      date.setDate(date.getDate() + i);
-      datList.push(date);
+    for (let d = startOfWeek; d <= endOfWeek; d.setDate(d.getDate() + 1)) {
+      datList.push(new Date(d));
     }
 
     setDates(datList);
   }, [currentDate]);
 
+  const getFormattedDate = (date: Date) => {
+    const month = date.toLocaleString("default", { month: "short" });
+    const day = date.getDate();
+
+    return `${month.slice(0, -1)} | ${day}`;
+  };
+
+  const setPreviousWeek = () => {
+    const previousWeek = new Date(currentDate);
+    previousWeek.setDate(previousWeek.getDate() - 7);
+
+    setCurrentDate(previousWeek.toISOString().split("T")[0]);
+  };
+
+  const setNextWeek = () => {
+    const nextWeek = new Date(currentDate);
+    nextWeek.setDate(nextWeek.getDate() + 7);
+
+    setCurrentDate(nextWeek.toISOString().split("T")[0]);
+  };
+
   return (
-    <ul>
-      {dates.map((date) => (
-        <li
-          onClick={() => setCurrentDate(date.toISOString().split("T")[0])}
-          style={{
-            backgroundColor:
-              currentDate === date.toISOString().split("T")[0]
-                ? "lightblue"
-                : "white",
-          }}
-        >
-          {date.toDateString()}
-        </li>
-      ))}
-    </ul>
+    <Wrapper>
+      <IconButtonWrapper onClick={setPreviousWeek}>
+        <Icon name="chevron_left" size="xlarge" />
+      </IconButtonWrapper>
+      <DateItemContainer>
+        {dates.map((date) => (
+          <DateItem
+            onClick={() => setCurrentDate(date.toISOString().split("T")[0])}
+            selected={currentDate === date.toISOString().split("T")[0]}
+            today={
+              new Date().toISOString().split("T")[0] ===
+              date.toISOString().split("T")[0]
+            }
+            data-weekday={date
+              .toLocaleString("default", { weekday: "short" })
+              .slice(0, -1)}
+          >
+            {getFormattedDate(date)}
+          </DateItem>
+        ))}
+      </DateItemContainer>
+      <IconButtonWrapper onClick={setNextWeek}>
+        <Icon name="chevron_right" size="xlarge" />
+      </IconButtonWrapper>
+    </Wrapper>
   );
 };
 
---
+++ src/components/DateSelector/styles.ts
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import { colors, rounding, spacing, typography } from "@styles";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${spacing.medium};
+  align-items: center;
+  padding: ${spacing.medium} 0 0 0;
+`;
+
+export const DateItemContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  gap: ${spacing.small};
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+export const DateItem = styled.li<{ selected: boolean; today: boolean }>`
+  position: relative;
+  gap: 8px;
+  align-items: center;
+  padding: ${spacing.small} 0;
+  width: 92px;
+  text-align: center;
+  border: 1px solid ${colors.neutral.lightest};
+  border-radius: ${rounding.large};
+  cursor: pointer;
+  text-transform: capitalize;
+
+  &::before {
+    content: attr(data-weekday);
+    position: absolute;
+    top: -18px;
+    left: 50%;
+    transform: translateX(-50%);
+    display: block;
+    text-align: center;
+
+    font-size: ${typography.sizing.xsmall};
+    color: ${colors.neutral.medium};
+    text-transform: uppercase;
+  }
+
+  ${({ selected }) =>
+    selected &&
+    `
+        background-color: ${colors.neutral.darkest};
+        color: ${colors.neutral.lightest};
+        border-color: ${colors.neutral.darkest};
+
+        &::before {
+                color: ${colors.neutral.darkest};
+        }
+    `};
+
+  ${({ today }) =>
+    today &&
+    `        
+        color: ${colors.danger.medium};
+
+    `};
+`;
+
+export const IconButtonWrapper = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 45px;
+  width: 45px;
+  border: none;
+  border-radius: ${rounding.large};
+  background-color: transparent;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${colors.neutral.lightest};
+  }
+
+  & > * {
+    cursor: pointer;
+  }
+`;
