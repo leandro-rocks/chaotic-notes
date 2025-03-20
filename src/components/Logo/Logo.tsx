c6adb9b16140a3dc2857d38f93942505bb63662f
LeandroPS <leandro.pires.souza@gmail.com>
2025-03-19 22:05:19 -0300

    feat: add Logo component with styled text for branding

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import { Text } from "./styles";
+
+const Logo = () => {
+  return <Text>Nice List</Text>;
+};
+
+export default Logo;
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import { colors, typography } from "@styles";
+import styled from "styled-components";
+
+export const Text = styled.span`
+  font-size: ${typography.sizing.xxlarge};
+  font-weight: ${typography.weight.bold};
+  color: ${colors.neutral.darkest};
+`;
